b55e00a324f4dccd72d87c5e07b6e06c219879f6
sidathrathnayake <sidathirathnayake@gmail.com>
2025-06-23 00:15:20 +0530

    Modify input field

---
+++ src/utils/constants/seo.constants.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Glow Event";
+export const SITE_DESCRIPTION = "AI-Powered Personalized Event Planner";
+export const SITE_KEYWORDS = "event planning, AI, personalized events, Glow Event";
